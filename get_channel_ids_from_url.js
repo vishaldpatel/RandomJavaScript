b153214bccdfa80fd10b0a512b05089b64f10e29
Vishal Patel <vishal@vishal.rs>
2025-07-28 13:10:35 -0700

    Fixed typos

--- get_channel_ids_from_url.js
+++ get_channel_ids_from_url.js
@@ -1,20 +1,18 @@
 // You want to get all the channel IDs that you are subscribed to on YouTube. You do:
 function getChannelIdFromURL(p_url) {
-  let rx = /(@.*)/g;
-  let arr = rx.exec(p_url);
+  let rx = /(@.*)/g
+  let arr = rx.exec(p_url)
   if (arr !== null && arr[1] !== null) {  
-    return arr[1];
-  } else {
-    return p_url;
+    return arr[1]
   }
 }
 
 let elems = document.getElementsByClassName("channel-link yt-simple-endpoint style-scope ytd-channel-renderer")
 let elem_arr = {}
-elem_arr[elems[0].href] = true;
+elem_arr[elems[0].href] = true
 for (let x = 1; x < elems.length; x++) { elem_arr[elems[x].href] = true }
 elem_arr = Object.keys(elem_arr)
-let elems_str = elem_arr[0];
+let elems_str = getChannelIdFromURL(elem_arr[0])
 for (let x = 1; x < elem_arr.length; x++) { elems_str = elems_str + ", " + getChannelIdFromURL(elem_arr[x]) }
 
 console.log(elems_str);
